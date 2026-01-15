bf94bd7d648dc038150483d6ac0d375ae26944fc
spandana-builds <spandana2400@gmail.com>
2026-01-15 17:23:05 +0530

    deployed on git pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Results from "./pages/Results.jsx";
 import Saved from "./pages/Saved.jsx";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/results" element={<Results />} />
         <Route path="/saved" element={<Saved />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
